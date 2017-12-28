ddbc339d562381659fb1b3ac7079cfd70be9dd14
Robert Sutton <rsutton@noojee.com.au>
2017-12-29 09:15:42 +1100

    clean up

--- src/test/java/ev3dev/sensors/slamtec/RPLidarA1Tests.java
+++ src/test/java/ev3dev/sensors/slamtec/RPLidarA1Tests.java
@@ -1,31 +1,32 @@
 package ev3dev.sensors.slamtec;
 
-import ev3dev.sensors.slamtec.model.Scan;
-import lombok.extern.slf4j.Slf4j;
-import org.junit.Ignore;
-import org.junit.Test;
-
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-public  @Slf4j class RPLidarA1Tests {
+import org.junit.Ignore;
+import org.junit.Test;
+
+import ev3dev.sensors.slamtec.model.Scan;
+
+public class RPLidarA1Tests
+{
 
 	@Test
 	@Ignore("Stream usage")
-	public void getDistanceStreamTest() throws Exception {
+	public void getDistanceStreamTest() throws Exception
+	{
 
 		final String USBPort = "ttyUSB0";
 		final RPLidarA1 lidar = new RPLidarA1(USBPort);
 		lidar.init();
-		lidar.scan().getDistances()
-			.stream()
-			.forEach(System.out::println);
+		lidar.scan().getDistances().stream().forEach(System.out::println);
 		lidar.close();
 	}
 
 	@Test
-	public void return360DistanceTest() throws Exception {
+	public void return360DistanceTest() throws Exception
+	{
 
 		final String USBPort = "ttyUSB0";
 		final RPLidarA1 lidar = new RPLidarA1(USBPort);
